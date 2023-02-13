902cbfcb897560386e6bc0fd331133644c120f4b
AlexanderZhao <zhaomengzhe@gmail.com>
2023-02-13 18:28:59 +0800

    feat(): update password utils test

--- src/utils/password.test.ts
+++ src/utils/password.test.ts
@@ -10,4 +10,10 @@ describe("password", () => {
       "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
     );
   });
+
+  it("should create random string from Uint8Array", () => {
+    const hash = plainTextToHash();
+
+    expect(hash).toBeTruthy();
+  });
 });
--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,4 +1,6 @@
 import { sha256 } from "@noble/hashes/sha256";
+import { randomBytes } from "@noble/hashes/utils";
 
-export const plainTextToHash = (plainText: string) =>
-  Buffer.from(sha256(plainText)).toString("hex");
+export const plainTextToHash = (
+  plainText: string | Uint8Array = randomBytes()
+) => Buffer.from(sha256(plainText)).toString("hex");
