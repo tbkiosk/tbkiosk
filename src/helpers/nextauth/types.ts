bdeb00966162d8ac8c8aef102fe0104dc7522b51
AlexanderZhao <zhaomengzhe@gmail.com>
2023-02-15 13:50:10 +0800

    feat(): abstract refresh token functions

---
+++ src/helpers/nextauth/types.ts
@@ -0,0 +1,20 @@
+import type { User, Session } from "next-auth";
+import type { JWT } from "next-auth/jwt";
+
+export type AuthToken = JWT & {
+  accessToken: string;
+  accessTokenExpires: string;
+  refreshToken: string;
+  user: User;
+  error?: string;
+};
+
+export type SessionType = {
+  session: ExtendedSession;
+  token: AuthToken;
+};
+
+type ExtendedSession = Session & {
+  accessToken?: string;
+  error?: string;
+};
