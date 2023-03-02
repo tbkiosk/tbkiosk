90afa45a867da1999907a187d055c494d4aecfc1
AlexanderZhao <zhaomengzhe@gmail.com>
2023-03-02 18:10:13 +0800

    fix(): forward Tooltip ref

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import cl from 'classnames'
 
 type TooltipProps = {
@@ -7,28 +8,26 @@ type TooltipProps = {
   position?: 'top' | 'bottom'
 }
 
-const Tooltip = ({
-  tip,
-  children,
-  classNames,
-  position = 'top',
-}: TooltipProps) => {
-  return (
-    <div className={cl(['group relative', classNames])}>
-      {tip && (
-        <span
-          className={cl([
-            'px-4 py-2 text-sm text-center whitespace-nowrap text-gray-100 rounded-md absolute left-1/2 bg-gray-800 invisible z-[1100] opacity-0 transition-opacity',
-            'group-hover:visible group-hover:opacity-90 -translate-x-1/2 -translate-y-[120%]',
-            position === 'bottom' && 'translate-y-[150%]',
-          ])}
-        >
-          {tip}
-        </span>
-      )}
-      {children}
-    </div>
-  )
-}
+const Tooltip = forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<TooltipProps>
+>(({ tip, children, classNames, position = 'top' }, ref) => (
+  <div className={cl(['group relative', classNames])} ref={ref}>
+    {tip && (
+      <span
+        className={cl([
+          'px-4 py-2 text-sm text-center whitespace-nowrap text-gray-100 rounded-md absolute left-1/2 bg-gray-800 invisible z-[1100] opacity-0 transition-opacity',
+          'group-hover:visible group-hover:opacity-90 -translate-x-1/2 -translate-y-[120%]',
+          position === 'bottom' && 'translate-y-[150%]',
+        ])}
+      >
+        {tip}
+      </span>
+    )}
+    {children}
+  </div>
+))
+
+Tooltip.displayName = 'Tooltip'
 
 export default Tooltip
