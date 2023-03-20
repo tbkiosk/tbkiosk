24a17563acb0bbeee3dc50b6ac64652850594703
AlexanderZhao <zhaomengzhe@gmail.com>
2023-03-20 12:08:46 +0800

    chore(): update deps

--- src/lib/wallet_connect.ts
+++ src/lib/wallet_connect.ts
@@ -1,4 +1,4 @@
-import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { configureChains, createClient } from 'wagmi'
 import { evmos, mainnet, polygon } from 'wagmi/chains'
 
@@ -6,17 +6,16 @@ const chains = [evmos, mainnet, polygon]
 
 // Wagmi client
 const { provider } = configureChains(chains, [
-  walletConnectProvider({
+  w3mProvider({
     projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
   }),
 ])
 
 export const wagmiClient = createClient({
   autoConnect: true,
-  connectors: modalConnectors({
+  connectors: w3mConnectors({
     projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
-    version: '2', // Minimum version of wagmi 0.11.3 is required to use version: "2"
-    appName: 'morphis-web3-modal',
+    version: 2, // Minimum version of wagmi 0.11.3 is required to use version: "2"
     chains,
   }),
   provider,
