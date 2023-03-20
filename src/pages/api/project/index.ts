13e4d982769d4d52da4df00a8b20ce6832636b08
AlexanderZhao <zhaomengzhe@gmail.com>
2023-03-20 16:25:17 +0800

    feat(): dev project POST api

---
+++ src/pages/api/project/index.ts
@@ -0,0 +1,86 @@
+import { getServerSession } from 'next-auth/next'
+import { ObjectId } from 'mongodb'
+
+import clientPromise from '@/lib/mongodb'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+
+import { projectFormSchema, projectDbSchema } from '@/schemas/project'
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { ResponseBase } from '@/types/response'
+import type { ExtendedSession } from '@/helpers/nextauth/types'
+import type { ProjectData } from '@/schemas/project'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseBase<ProjectData[] | null>>) => {
+  const session: ExtendedSession | null = await getServerSession(req, res, authOptions)
+  if (!session) {
+    return res.status(401).json({
+      message: '',
+    })
+  }
+
+  if (!session.user?.id) {
+    return res.status(500).json({
+      message: 'Missing user in session, try to sign out and sign in again',
+    })
+  }
+
+  const client = await clientPromise
+  const db = client.db(`${process.env.NODE_ENV}`)
+  const collection = db.collection<ProjectData>('project')
+
+  /**
+   * @method GET
+   * @returns created projects
+   */
+  if (req.method === 'GET') {
+    const {
+      query: { creatorId },
+    } = req
+    return res.status(200).json({
+      data: [],
+    })
+  }
+
+  /**
+   * @method POST
+   * @returns create a new project
+   */
+  if (req.method === 'POST') {
+    const { error: formSchemaError } = projectFormSchema.validate(req.body)
+    if (formSchemaError) {
+      return res.status(400).send({
+        message: formSchemaError.message || 'Invalid project options',
+      })
+    }
+
+    const now = +new Date()
+    const transformedData: ProjectData = { ...req.body, createdTime: now, updatedTime: now, creatorId: session.user.id }
+
+    const { error: dbSchemaError } = projectDbSchema.validate(transformedData)
+    if (dbSchemaError) {
+      return res.status(500).send({
+        message: dbSchemaError.message || 'Failed to transform project data',
+      })
+    }
+
+    try {
+      await collection.insertOne({ ...transformedData, creatorId: new ObjectId(transformedData.creatorId) })
+
+      return res.status(200).json({
+        data: null,
+        message: `Created project project ${transformedData.projectName}`,
+      })
+    } catch (err) {
+      return res.status(500).json({
+        message: err.message,
+      })
+    }
+  }
+
+  return res.status(405).json({
+    message: '',
+  })
+}
+
+export default handler
