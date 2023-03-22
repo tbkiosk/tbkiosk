e9b154dd3fb06a5a603f951b22ab61475a553167
AlexanderZhao <zhaomengzhe@gmail.com>
2023-03-22 18:43:56 +0800

    update(): put cos bucket and region to constants

---
+++ src/constants/cos.ts
@@ -0,0 +1,4 @@
+export const TENCENT_COS_BUCKET = 'morphis-img-dev-1256253626'
+export const TENCENT_COS_TEMP_BUCKET = 'morphis-img-temp-1256253626'
+export const TENCENT_COS_REGION = 'ap-singapore'
+export const TENCENT_COS_CDN_DOMAIN = 'morphis-img-temp-1256253626.cos.accelerate.myqcloud.com'
--- src/utils/cos.ts
+++ src/utils/cos.ts
@@ -1,47 +1,46 @@
 import STS from 'qcloud-cos-sts'
 
-const DEFAULT_TEMP_CREDENTIAL_DURATION = 1800
+import { TENCENT_COS_TEMP_BUCKET, TENCENT_COS_REGION } from '@/constants/cos'
 
-const CONFIG = {
-  secretId: (process.env.TENCENT_COS_SECRET_ID as string) || 'AKIDyhCRZIXKXw71g1QLH5ey0e0iCPKttJ9k',
-  secretKey: (process.env.TENCENT_COS_SECRET_KEY as string) || 'GNGtfEXkh17weDwShgAQRAUggfRQ9euu',
-  durationSeconds: DEFAULT_TEMP_CREDENTIAL_DURATION,
-  bucket: (process.env.TENCENT_COS_BUCKET as string) || 'morphis-img-dev-1256253626',
-  region: (process.env.TENCENT_COS_REGION as string) || 'ap-singapore',
-  allowPrefix: '',
+const DEFAULT_TEMP_CREDENTIAL_DURATION = 180
+
+type GetCredential = {
+  bucket?: string
 }
 
-const shortBucketName = CONFIG.bucket.substring(0, CONFIG.bucket.lastIndexOf('-'))
-const appId = CONFIG.bucket.substring(1 + CONFIG.bucket.lastIndexOf('-'))
-
-const POLICY = {
-  version: '2.0',
-  statement: [
-    {
-      action: [
-        'name/cos:PutObject',
-        'name/cos:PostObject',
-        'name/cos:InitiateMultipartUpload',
-        'name/cos:ListMultipartUploads',
-        'name/cos:ListParts',
-        'name/cos:UploadPart',
-        'name/cos:CompleteMultipartUpload',
-      ],
-      effect: 'allow',
-      principal: { qcs: ['*'] },
-      resource: ['qcs::cos:' + CONFIG.region + ':uid/' + appId + ':prefix//' + appId + '/' + shortBucketName + '/' + CONFIG.allowPrefix],
-    },
-  ],
-}
-
-export const getCredential = async (): Promise<STS.CredentialData> =>
+export const getCredential = async ({ bucket }: GetCredential = {}): Promise<STS.CredentialData> =>
   new Promise((res, rej) => {
+    const _bucket = bucket || TENCENT_COS_TEMP_BUCKET
+    const shortBucketName = _bucket.substring(0, _bucket.lastIndexOf('-'))
+    const appId = _bucket.substring(1 + _bucket.lastIndexOf('-'))
+    const allowPrefix = '*'
+
     STS.getCredential(
       {
-        secretId: CONFIG.secretId,
-        secretKey: CONFIG.secretKey,
-        durationSeconds: CONFIG.durationSeconds,
-        policy: POLICY,
+        secretId: process.env.TENCENT_COS_SECRET_ID as string,
+        secretKey: process.env.TENCENT_COS_SECRET_KEY as string,
+        durationSeconds: DEFAULT_TEMP_CREDENTIAL_DURATION,
+        policy: {
+          version: '2.0',
+          statement: [
+            {
+              action: [
+                'name/cos:PutObject',
+                'name/cos:PostObject',
+                'name/cos:InitiateMultipartUpload',
+                'name/cos:ListMultipartUploads',
+                'name/cos:ListParts',
+                'name/cos:UploadPart',
+                'name/cos:CompleteMultipartUpload',
+              ],
+              effect: 'allow',
+              principal: { qcs: ['*'] },
+              resource: [
+                'qcs::cos:' + TENCENT_COS_REGION + ':uid/' + appId + ':prefix//' + appId + '/' + shortBucketName + '/' + allowPrefix,
+              ],
+            },
+          ],
+        },
       },
       function (err, credential) {
         if (err) {
