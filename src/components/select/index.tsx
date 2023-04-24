b91b86c044601b8cab4fdf30a4eafb8164518a6d
AlexanderZhao <zhaomengzhe@gmail.com>
2023-04-24 16:38:40 +0800

    feat(): add arrow in Select

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -14,53 +14,65 @@ type SelectProps = {
   options: Option[]
   className?: string
   buttonClassName?: string
+  showArrow?: boolean
 }
 
 // export const Select = forwardRef<HTMLSelectElement, SelectProps>()
 
-export const Select = ({ options, value, onChange, className, buttonClassName }: SelectProps) => {
+export const Select = ({ options, value, onChange, className, buttonClassName, showArrow }: SelectProps) => {
   return (
     <Listbox
       value={options.find(_option => _option.id === value)}
       onChange={onChange}
     >
-      <div className={cl(['relative w-full', className])}>
-        <Listbox.Button
-          className={cl([
-            'block w-full px-2 py-2 text-sm text-gray-900 placeholder:text-gray-400 bg-white border border-black rounded-md truncate shadow-sm',
-            'transition-colors hover:bg-neutral-100',
-            buttonClassName,
-          ])}
-        >
-          {value}
-        </Listbox.Button>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Listbox.Options className={cl(['absolute max-h-60 w-full mt-1 py-1 overflow-auto bg-white rounded-md shadow-lg'])}>
-            {options.map(_option => (
-              <Listbox.Option
-                className={({ active }) =>
-                  cl(['relative cursor-default select-none py-2 px-2 text-sm', active ? 'bg-neutral-200 text-amber-900' : 'text-gray-900'])
-                }
-                key={_option.id}
-                value={_option}
-                disabled={_option.disabled ?? false}
-              >
-                {({ selected }) => (
-                  <>
-                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{_option.name}</span>
-                    {selected ? <span className="absolute px-2 inset-y-0 left-0 flex items-center text-amber-600"></span> : null}
-                  </>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Transition>
-      </div>
+      {({ open }) => (
+        <div className={cl(['relative w-full', className])}>
+          <Listbox.Button
+            className={cl([
+              'block w-full px-2 py-2 text-sm text-gray-900 placeholder:text-gray-400 bg-white border border-black rounded-md truncate shadow-sm',
+              'transition-colors hover:bg-neutral-100',
+              buttonClassName,
+              showArrow && 'relative pr-8',
+            ])}
+          >
+            {value}
+            {showArrow && (
+              <span className={cl(['absolute inset-y-0 right-4 flex flex-col justify-center transition-transform', open && 'rotate-180'])}>
+                <i className="fa-solid fa-chevron-up" />
+              </span>
+            )}
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className={cl(['absolute max-h-60 w-full mt-1 py-1 overflow-auto bg-white rounded-md shadow-lg'])}>
+              {options.map(_option => (
+                <Listbox.Option
+                  className={({ active }) =>
+                    cl([
+                      'relative cursor-pointer select-none py-2 px-2 text-sm',
+                      active ? 'bg-neutral-200 text-amber-900' : 'text-gray-900',
+                    ])
+                  }
+                  key={_option.id}
+                  value={_option}
+                  disabled={_option.disabled ?? false}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{_option.name}</span>
+                      {selected ? <span className="absolute px-2 inset-y-0 left-0 flex items-center text-amber-600"></span> : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      )}
     </Listbox>
   )
 }
