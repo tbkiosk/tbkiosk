fcf5737a995e93aa93d9e47a819e155f0fb43261
AlexanderZhao <zhaomengzhe@gmail.com>
2023-05-16 18:37:17 +0800

    fix(): update accounts type, to save discord oauth authorization

--- src/schemas/accounts.ts
+++ src/schemas/accounts.ts
@@ -6,4 +6,8 @@ export const ACCOUNTS_TABLE = 'accounts'
 export type AccountData = Omit<Account, 'userId'> & {
   userId: ObjectId
   discord_access_token?: string
+  discord_refresh_token?: string
+  discord_expires_at?: number
+  discord_scope?: string
+  discord_token_type?: string
 }
