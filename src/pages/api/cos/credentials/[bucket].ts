e0002da06b0e213335bd3ac11f83924d53040f05
AlexanderZhao <zhaomengzhe@gmail.com>
2023-06-05 14:46:01 +0800

    chore(): remove outdated APIs

--- src/pages/api/cos/credentials/[bucket].ts
+++ src/pages/api/cos/credentials/[bucket].ts
@@ -1,7 +1,4 @@
-import { getServerSession } from 'next-auth/next'
 import STS from 'qcloud-cos-sts'
-
-import { authOptions } from '@/pages/api/auth/[...nextauth]'
 
 import { getCredential } from '@/utils/cos'
 
@@ -11,19 +8,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import type { ResponseBase } from '@/types/response'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseBase<STS.CredentialData>>) => {
-  const session = await getServerSession(req, res, authOptions)
-  if (!session) {
-    return res.status(401).json({
-      message: '',
-    })
-  }
-
-  if (!session?.user?.email) {
-    return res.status(500).json({
-      message: 'Missing user email in session, try to sign out and login',
-    })
-  }
-
   /**
    * @method GET
    * @returns user with discord or twitter email by provider in users collection
