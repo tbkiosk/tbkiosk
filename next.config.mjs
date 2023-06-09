9e0a6ad7479ce70889f5c7581c818eb5147c25ab
AlexanderZhao <zhaomengzhe@gmail.com>
2023-06-09 17:02:00 +0800

    feat(): config and add default avatar

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'placehold.co',
       },
     ],
   },
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,19 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useSession } from 'next-auth/react'
 import cx from 'classix'
+
+import { Loading } from '@/components'
 
 type LayoutProps = {
   children?: React.ReactNode | React.ReactNode[]
   className?: string
+  ignoreSession?: boolean
 }
 
 const Layout = ({ children, className }: LayoutProps) => {
+  const { data, status } = useSession()
+
   return (
     <div className={cx('h-full w-full flex flex-col', className)}>
       <header className="flex justify-between px-[160px] py-[40px]">
@@ -20,8 +26,18 @@ const Layout = ({ children, className }: LayoutProps) => {
             width="160"
           />
         </Link>
-        <div className="flex items-center gap-2">
-          <div className="h-[40px] w-[40px] rounded-full bg-gray-200" />
+        <div className="min-w-[40px] flex items-center gap-2">
+          <Loading isLoading={status === 'loading'}>
+            <>
+              <Image
+                alt="avatar"
+                className="rounded-full"
+                height="40"
+                src={data?.user?.image || 'https://placehold.co/40x40/dddddd/dddddd/png'}
+                width="40"
+              />
+            </>
+          </Loading>
         </div>
       </header>
       <main className="px-[160px]">{children}</main>
