34a079ff869d7d7bbaf9dbfc75645f46bbad875e
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-11 11:41:18 +0800

    chore(): remove storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  extends: [
-    '@morphis-labs/eslint-config',
-    'plugin:@next/next/recommended',
-    'plugin:storybook/recommended',
-    'plugin:@tanstack/eslint-plugin-query/recommended',
-  ],
+  extends: ['@morphis-labs/eslint-config', 'plugin:@next/next/recommended', 'plugin:@tanstack/eslint-plugin-query/recommended'],
   rules: {
     '@tanstack/query/exhaustive-deps': 'off',
   },
