585c0580a5703743245e20d21f1fd0ca4a2f0c8d
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-11 19:14:27 +0800

    refactor(): update login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,36 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useSearchParams } from 'next/navigation'
+import { useSession, getCsrfToken, signIn } from 'next-auth/react'
+import { SiweMessage } from 'siwe'
+import { useAccount, useNetwork, useSignMessage } from 'wagmi'
+import { useConnectModal } from '@rainbow-me/rainbowkit'
+import { useWallet, ConnectModal } from '@suiet/wallet-kit'
+import { Flex, Modal, Title, Text, Tooltip, Divider, useMantineColorScheme, rem, Button } from '@mantine/core'
+import { notifications } from '@mantine/notifications'
 import cx from 'classix'
 
-import WalletConnectModal from '@/components/__login__/wallet_connect_modal'
+import { ellipsisMiddle } from '@/utils/address'
+
+import Logo from '@/assets/icons/logo'
+import WalletIcon from '@/assets/icons/wallet'
 
 const Login = () => {
   const { data: session } = useSession()
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const { colorScheme } = useMantineColorScheme()
 
   useEffect(() => {
     if (session) {
       router.push('/discover')
     }
   }, [session, router])
+
+  const showSocialsLogin = searchParams.get('socials')
 
   return (
     <>
@@ -26,48 +41,252 @@ const Login = () => {
           content="Morphis Airdawg login"
         />
       </Head>
-      <div className={cx('h-full w-full flex flex-col')}>
-        <WalletConnectModal
-          open={true}
-          setOpen={() => null}
-        />
-        <header className="flex justify-between px-[160px] py-10 blur-sm">
-          <Image
-            alt="logo"
-            height="40"
-            priority
-            src="/icons/logo_with_text.svg"
-            width="160"
-          />
-          <div className="min-w-[40px] flex items-center gap-2">
-            <>
-              <Image
-                alt="avatar"
-                className="rounded-full"
-                height="40"
-                src="https://placehold.co/40x40/dddddd/dddddd/png"
-                width="40"
-              />
-            </>
-          </div>
-        </header>
-        <main className="flex flex-col px-[160px] py-8 blur-sm">
-          <div className="h-[360px] w-full mb-8 bg-blue-200 rounded-2xl"></div>
-          <p className="flex items-center gap-2 mb-4 font-bold text-2xl">
-            Lorem ipsum dolor
-            <span className="w-6 h-6 bg-yellow-400 rounded-full"></span>
-          </p>
-          <p className="flex gap-4">
-            <span>Lorem</span>
-            <span>ipsum</span>
-            <span>doloripsum</span>
-            <span>test</span>
-            <span>abc</span>
-          </p>
-        </main>
-      </div>
+      <main
+        className={cx(`h-[100vh] overflow-hidden bg-[url('/images/login/bg_overlay.jpg')] bg-cover`, colorScheme === 'dark' && 'invert')}
+      >
+        <Modal
+          centered
+          closeOnClickOutside={false}
+          onClose={() => null}
+          opened
+          radius="lg"
+          withCloseButton={false}
+        >
+          <Flex
+            align="center"
+            direction="column"
+            gap={rem(16)}
+          >
+            <Logo />
+            <Title
+              fw={500}
+              order={3}
+            >
+              Find web3 perks with AirDawg
+            </Title>
+            <Image
+              alt=""
+              height={156}
+              src="/images/login/artwork_4.png"
+              width={274}
+            />
+            {showSocialsLogin && (
+              <>
+                <Button
+                  classNames={{
+                    inner: 'justify-start',
+                  }}
+                  color="gray"
+                  fw={400}
+                  leftIcon={<i className="fa-brands fa-twitter text-2xl text-[#1D9BF0]" />}
+                  onClick={() => signIn('twitter', { callbackUrl: '/login' })}
+                  pl={72}
+                  radius="xl"
+                  size="lg"
+                  variant="default"
+                  w={304}
+                >
+                  Login with Twitter
+                </Button>
+                <Button
+                  classNames={{
+                    inner: 'justify-start',
+                  }}
+                  color="gray"
+                  fw={400}
+                  leftIcon={<i className="fa-brands fa-discord text-xl text-[#7289da]" />}
+                  onClick={() => signIn('discord', { callbackUrl: '/login' })}
+                  pl={72}
+                  radius="xl"
+                  size="lg"
+                  variant="default"
+                  w={304}
+                >
+                  Login with Discord
+                </Button>
+                <Divider
+                  className="w-[304px]"
+                  label="Or"
+                  labelPosition="center"
+                  my="xs"
+                />
+              </>
+            )}
+            <EthereumSignButton />
+            <SuiSignButton />
+            <Tooltip
+              label="Coming soon"
+              withArrow
+            >
+              <div>
+                <Button
+                  classNames={{
+                    inner: 'justify-start',
+                  }}
+                  color="gray"
+                  disabled
+                  fw={400}
+                  leftIcon={<WalletIcon />}
+                  pl={72}
+                  radius="xl"
+                  size="lg"
+                  variant="default"
+                  w={304}
+                >
+                  Solana Wallet
+                </Button>
+              </div>
+            </Tooltip>
+            <Text>
+              Have an account?{' '}
+              <Link
+                href="/login?socials=true"
+                className="underline"
+              >
+                Log in instead
+              </Link>
+            </Text>
+          </Flex>
+        </Modal>
+      </main>
     </>
   )
 }
 
 export default Login
+
+const EthereumSignButton = () => {
+  const { isConnected, address } = useAccount()
+  const { chain } = useNetwork()
+  const { signMessageAsync } = useSignMessage()
+  const { openConnectModal } = useConnectModal()
+
+  const onConnect = async () => {
+    if (!isConnected) {
+      openConnectModal?.()
+      return
+    }
+
+    try {
+      const message = new SiweMessage({
+        domain: window.location.host,
+        address,
+        statement: 'Sign in with Ethereum to Morphis Airdawg',
+        uri: window.location.origin,
+        version: '1',
+        chainId: chain?.id,
+        nonce: await getCsrfToken(),
+      })
+      const signature = await signMessageAsync({
+        message: message.prepareMessage(),
+      })
+
+      signIn('Ethereum', {
+        message: JSON.stringify(message),
+        redirect: true,
+        signature,
+        callbackUrl: '/discover',
+      })
+    } catch (error) {
+      notifications.show({
+        color: 'red',
+        message: (error as Error)?.message,
+        title: 'Failed to sign in',
+      })
+    }
+  }
+
+  return (
+    <Button
+      classNames={{
+        inner: 'justify-start',
+      }}
+      color="gray"
+      fw={400}
+      leftIcon={<WalletIcon />}
+      onClick={() => onConnect()}
+      pl={72}
+      radius="xl"
+      size="lg"
+      variant="default"
+      w={304}
+    >
+      Ethereum Wallet
+    </Button>
+  )
+}
+
+const SuiSignButton = () => {
+  const { connected, address, disconnect, signMessage } = useWallet()
+
+  const [showModal, setShowModal] = useState(false)
+
+  const onConnect = async () => {
+    if (!connected) {
+      setShowModal(true)
+      return
+    }
+
+    try {
+      const message = JSON.stringify({
+        domain: window.location.host,
+        address,
+        statement: 'Sign in with Sui to Morphis Airdawg',
+        uri: window.location.origin,
+        nonce: await getCsrfToken(),
+      })
+      const msgBytes = new TextEncoder().encode(message)
+      const signature = await signMessage({
+        message: msgBytes,
+      })
+
+      signIn('Sui', {
+        message,
+        redirect: true,
+        signature: JSON.stringify(signature),
+        callbackUrl: '/discover',
+      })
+    } catch (error) {
+      notifications.show({
+        color: 'red',
+        message: (error as Error)?.message,
+        title: 'Failed to sign in',
+      })
+    }
+  }
+
+  return (
+    <>
+      <ConnectModal
+        onConnectSuccess={() => setShowModal(false)}
+        onOpenChange={open => setShowModal(open)}
+        open={showModal}
+      />
+      <Button
+        classNames={{
+          inner: 'justify-start',
+        }}
+        color="gray"
+        fw={400}
+        leftIcon={<WalletIcon />}
+        onClick={() => onConnect()}
+        pl={72}
+        radius="xl"
+        size="lg"
+        variant="default"
+        w={304}
+      >
+        {connected ? ellipsisMiddle(address || '') : 'Sui Wallet'}
+        {connected && (
+          <i
+            className="fa-regular fa-circle-xmark cursor-pointer transition hover:scale-[1.1] hover:opacity-70"
+            onClick={e => {
+              e.stopPropagation()
+              disconnect()
+            }}
+          />
+        )}
+      </Button>
+    </>
+  )
+}
