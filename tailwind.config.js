8be0abaa9b376d8ae0e5c8037fc28f832e572cc2
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-20 09:43:50 +0800

    chore(): update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require('tailwindcss/colors')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}', './src/layouts/**/*.{js,ts,jsx,tsx}'],
