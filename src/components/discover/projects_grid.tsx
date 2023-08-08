6026168ab6ac949c030950bea5815583c2d76c28
AlexanderZhao <zhaomengzhe@gmail.com>
2023-08-08 13:01:15 +0800

    feat(): dev ProjectsGrid

---
+++ src/components/discover/projects_grid.tsx
@@ -0,0 +1,76 @@
+import Link from 'next/link'
+import { Grid, LoadingOverlay, rem } from '@mantine/core'
+import { notifications } from '@mantine/notifications'
+import { useQuery } from '@tanstack/react-query'
+
+import ProjectCard from './project_card'
+
+import { request } from '@/utils/request'
+
+import type { Project } from '@prisma/client'
+
+type ProjectsGridProps = {
+  limit?: number
+  pageSize?: number
+  pageNumber?: number
+}
+
+const ProjectsGrid = ({ limit, pageNumber, pageSize }: ProjectsGridProps) => {
+  const { data, isLoading } = useQuery<Project[], Error>({
+    queryKey: ['discover'],
+    queryFn: async () => {
+      const { data, error } = await request<Project[], string>({
+        url: '/api/discover',
+        params: {
+          limit,
+          pageNumber,
+          pageSize,
+        },
+      })
+
+      if (error) {
+        throw new Error(error)
+      }
+
+      return data || []
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        color: 'red',
+        message: error.message,
+        title: 'Error',
+        withCloseButton: true,
+      })
+    },
+    retry: false,
+    refetchOnWindowFocus: false,
+  })
+
+  return (
+    <Grid
+      gutter="xl"
+      maw={rem(1440)}
+      mb={rem(48)}
+      pos="relative"
+    >
+      <LoadingOverlay visible={isLoading} />
+      {data?.map(_project => (
+        <Grid.Col
+          key={_project.id}
+          md={3}
+          sm={4}
+          xs={6}
+        >
+          <Link
+            href={`/discover/${_project.id}`}
+            passHref
+          >
+            <ProjectCard {..._project} />
+          </Link>
+        </Grid.Col>
+      ))}
+    </Grid>
+  )
+}
+
+export default ProjectsGrid
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Fonts } from './fonts'
 
 export { default as FavoriteButton } from './discover/favorite_button'
 export { default as ProjectCard } from './discover/project_card'
+export { default as ProjectsGrid } from './discover/projects_grid'
