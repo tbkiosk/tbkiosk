52654b5675cfba1a1f84835d1b06760273baef83
AlexanderZhao <zhaomengzhe@gmail.com>
2023-08-08 11:11:56 +0800

    fix(): fix FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,34 +1,36 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { createGetInitialProps } from '@mantine/next'
 
 export const getInitialProps = createGetInitialProps()
 
-const AppDocument = () => {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta
-          name="description"
-          content="kiosk"
-        />
-        <meta charSet="UTF-8" />
-        <link
-          rel="icon"
-          href="/favicon.ico?v=2"
-        />
-        <script
-          crossOrigin="anonymous"
-          defer
-          src="https://kit.fontawesome.com/205d88d001.js"
-        />
-      </Head>
-      <body>
-        <Main />
-        <div id="portal" />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps
 
-export default AppDocument
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="kiosk"
+          />
+          <meta charSet="UTF-8" />
+          <link
+            rel="icon"
+            href="/favicon.ico?v=2"
+          />
+          <script
+            crossOrigin="anonymous"
+            defer
+            src="https://kit.fontawesome.com/205d88d001.js"
+          />
+        </Head>
+        <body>
+          <Main />
+          <div id="portal" />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
