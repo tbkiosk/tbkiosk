9dae40c873db4f005b181947340e19d21ceb4009
AlexanderZhao <zhaomengzhe@gmail.com>
2023-09-04 18:01:18 +0800

    feat(): beep settings page

--- app/mint/beep/settings/page.tsx
+++ app/mint/beep/settings/page.tsx
@@ -1,23 +1,5 @@
-import { AppShell } from '@mantine/core'
-
-import Header from './components/header'
 import Main from './components/main'
 
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Kiosk - Beep Settings',
-}
-
-export default function Beep() {
-  return (
-    <AppShell
-      h="100%"
-      header={{ height: 72 }}
-      padding={0}
-    >
-      <Header />
-      <Main />
-    </AppShell>
-  )
+export default function BeepSettingsPage() {
+  return <Main />
 }
