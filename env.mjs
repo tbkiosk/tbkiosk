f9ccbda5bf75ceff805ab8f24823e84f57483e23
AlexanderZhao <zhaomengzhe@gmail.com>
2023-09-07 18:52:26 +0800

    chore(): update env

--- env.mjs
+++ env.mjs
@@ -3,30 +3,14 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    NEXTAUTH_URL: z.string().min(1),
-    NEXTAUTH_SECRET: z.string().min(1),
     DATABASE_URL: z.string().min(1),
-    NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID: z.string().min(1),
-    TWITTER_CLIENT_ID: z.string().min(1),
-    TWITTER_CLIENT_SECRET: z.string().min(1),
-    DISCORD_CLIENT_ID: z.string().min(1),
-    DISCORD_CLIENT_SECRET: z.string().min(1),
-    AWS_S3_SECRET_ID: z.string().min(1),
-    AWS_S3_SECRET_KEY: z.string().min(1),
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID: z.string().min(1),
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: z.string().min(1),
   },
   runtimeEnv: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
-    TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID,
-    TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    AWS_S3_SECRET_ID: process.env.AWS_S3_SECRET_ID,
-    AWS_S3_SECRET_KEY: process.env.AWS_S3_SECRET_KEY,
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
   },
 })
