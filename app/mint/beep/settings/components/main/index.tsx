63d78153aa2b1753ff04a7a8d28b191aa708d67f
AlexanderZhao <zhaomengzhe@gmail.com>
2023-09-09 18:21:24 +0800

    feat(): add nft balance

--- app/mint/beep/settings/components/main/index.tsx
+++ app/mint/beep/settings/components/main/index.tsx
@@ -2,11 +2,12 @@
 
 import { useEffect } from 'react'
 import Link from 'next/link'
-import { AppShell, Box, Container, Title, LoadingOverlay, Image, Text } from '@mantine/core'
+import { AppShell, Box, Container, Title, LoadingOverlay, Image, Text, Loader } from '@mantine/core'
 import { notifications } from '@mantine/notifications'
 import { useChainId, useAddress, useContract, useOwnedNFTs, useSigner } from '@thirdweb-dev/react'
 import { TokenboundClient } from '@tokenbound/sdk'
 import { useQuery } from '@tanstack/react-query'
+import { useBalance } from 'wagmi'
 import { cx } from 'classix'
 
 import { BeepContractAddress, CONTRACT_ADDRESS } from 'constants/beep'
@@ -77,47 +78,92 @@ function AccountInfo({ tokenId }: { tokenId: string }) {
 
   const tokenboundClient = new TokenboundClient({ signer: signer, chainId: chain.chainId })
 
-  if (status !== 'Deployed') return null
-
   const tokenBoundAccount = tokenboundClient.getAccount({
     tokenContract: CONTRACT_ADDRESS,
     tokenId,
   })
 
-  return (
-    <Box className={classes['account-info-container']}>
-      <Text className={cx(classes['account-info-text'], classes.id)}>
-        <Text component="span">#</Text>
-        {tokenId}
-      </Text>
-      <AccountStatus tokenBoundAccount={tokenBoundAccount} />
-    </Box>
-  )
-}
+  const { data } = useBalance({
+    address: tokenBoundAccount,
+  })
 
-function AccountStatus({ tokenBoundAccount }: { tokenBoundAccount: string }) {
-  const { data: profile, isLoading } = useQuery<{ name: string; description: string; image: string }>({
-    enabled: false,
-    queryKey: ['token-bound-account-profile'],
+  // const { data: usdcData } = useBalance({
+  //   address: tokenBoundAccount,
+  //   token: '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
+  // })
+
+  const { data: price } = useQuery<{ USD: number }>({
+    enabled: !!data,
+    queryKey: ['eth-price'],
     queryFn: async () => {
-      const res = await fetch(`/api/beep/profile/${tokenBoundAccount}`)
+      const res = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD`)
 
       if (!res.ok) {
         throw new Error(res.statusText)
       }
 
-      const projects = await res.json()
-      return projects
+      const price = await res.json()
+      return price
     },
   })
 
-  if (isLoading) return null
+  if (status !== 'Deployed') return null
 
   return (
-    <Image
-      alt="switch"
-      className={classes['switch-icon']}
-      src={profile ? '/icons/beep-bot-on.svg' : '/icons/beep-bot-off.svg'}
-    />
+    <Box className={classes['account-info-container']}>
+      <Text className={classes['account-info-text']}>
+        <Text component="span">#</Text>
+        {tokenId}
+      </Text>
+      {/* <AccountStatus tokenBoundAccount={tokenBoundAccount} /> */}
+      <Text className={cx(classes['account-info-text'], classes.symbol)}>ETH</Text>
+      <Text className={cx(classes['account-info-text'], classes.balance)}>
+        {data?.formatted || (
+          <Loader
+            color="blue"
+            type="bars"
+          />
+        )}
+      </Text>
+      <Text className={cx(classes['account-info-text'], classes.price)}>
+        ($
+        {data?.formatted && price ? (
+          Number(data.formatted) * price.USD ?? '-'
+        ) : (
+          <Loader
+            color="blue"
+            type="bars"
+          />
+        )}
+        )
+      </Text>
+    </Box>
   )
 }
+
+// function AccountStatus({ tokenBoundAccount }: { tokenBoundAccount: string }) {
+//   const { data: profile, isLoading } = useQuery<{ name: string; description: string; image: string }>({
+//     enabled: false,
+//     queryKey: ['token-bound-account-profile'],
+//     queryFn: async () => {
+//       const res = await fetch(`/api/beep/profile/${tokenBoundAccount}`)
+
+//       if (!res.ok) {
+//         throw new Error(res.statusText)
+//       }
+
+//       const projects = await res.json()
+//       return projects
+//     },
+//   })
+
+//   if (isLoading) return null
+
+//   return (
+//     <Image
+//       alt="switch"
+//       className={classes['switch-icon']}
+//       src={profile ? '/icons/beep-bot-on.svg' : '/icons/beep-bot-off.svg'}
+//     />
+//   )
+// }
