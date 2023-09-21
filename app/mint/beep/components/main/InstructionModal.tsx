fba0b0e4394285a762b61cf6b0f9095f35cd8f27
maxxmorphis <max.x.morphis@gmail.com>
2023-09-21 14:49:35 +0800

    fix(): modal header style issue

--- app/mint/beep/components/main/InstructionModal.tsx
+++ app/mint/beep/components/main/InstructionModal.tsx
@@ -59,9 +59,12 @@ export const InstructionModal = ({ isOpen, onClose }: Props) => {
           padding: 32,
         },
         header: {
-          backgroundColor: '#FFF',
+          backgroundColor: 'transparent',
           padding: 0,
         },
+      }}
+      classNames={{
+        header: cx('mantine-Modal-header', classes['modal-header']),
       }}
     >
       <Center mb={32}>
