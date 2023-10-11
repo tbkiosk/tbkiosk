8079a08c7a2cb191b5fccbcddf6e033722589a73
AlexanderZhao <zhaomengzhe@gmail.com>
2023-10-11 15:53:56 +0800

    feat(): beep settings page

---
+++ src/app/mint/beep/settings/page.tsx
@@ -0,0 +1,10 @@
+import BeepsGrid from './components/beeps_grid'
+
+const BeepSettings = () => (
+  <main className="max-w-screen-2xl mx-auto px-4 md:px-8 py-2 pt-16 text-white">
+    <h1 className="mb-4 font-bold text-2xl">My Beeps</h1>
+    <BeepsGrid />
+  </main>
+)
+
+export default BeepSettings
