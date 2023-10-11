d06120adbb4f362a8bded41a607ccefa3170fd8f
AlexanderZhao <zhaomengzhe@gmail.com>
2023-10-11 15:53:56 +0800

    feat(): beep settings page

---
+++ src/app/mint/beep/settings/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from '@/components/loader'
+
+const Loading = () => (
+  <div className="h-[100vh] flex items-center justify-center">
+    <Loader />
+  </div>
+)
+
+export default Loading
