55a3afc4ab02e8da8bf4bd20fa78c01f654a9a0d
AlexanderZhao <zhaomengzhe@gmail.com>
2023-10-13 13:08:19 +0800

    feat(): add beep hooks

---
+++ src/hooks/use_get_tba_account.ts
@@ -0,0 +1,22 @@
+import { useSigner } from '@thirdweb-dev/react'
+import { TokenboundClient } from '@tokenbound/sdk'
+
+import { chain } from '../constants/chain'
+
+type Props = {
+  tokenId: string
+  contractAddress: `0x${string}`
+  implementationAddress: `0x${string}`
+}
+
+export const useGetTbaAccount = ({ tokenId, contractAddress, implementationAddress }: Props) => {
+  const signer = useSigner()
+
+  const tokenboundClient = new TokenboundClient({ signer: signer, chainId: chain.chainId })
+
+  return tokenboundClient.getAccount({
+    tokenContract: contractAddress,
+    tokenId: tokenId,
+    implementationAddress: implementationAddress,
+  })
+}
