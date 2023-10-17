918b46506db776ee66f4e49028363e658f694259
AlexanderZhao <zhaomengzhe@gmail.com>
2023-10-17 18:45:05 +0800

    feat(): add Switch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,6 @@ import { nextui } from '@nextui-org/theme'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: [
-    './node_modules/@nextui-org/theme/dist/components/(button|input|image|spinner|select|dropdown|modal|table).js',
-    './src/components/**/*.{ts,tsx}',
-    './src/app/**/*.{ts,tsx}',
-  ],
+  content: ['./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{ts,tsx}', './src/app/**/*.{ts,tsx}'],
   plugins: [require('tailwindcss-animate'), nextui()],
 }
