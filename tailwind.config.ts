1312e988bac666a86c0e9c6f2360ca29f083c60a
AlexanderZhao <zhaomengzhe@gmail.com>
2023-10-17 15:21:55 +0800

    feat(): add next-table

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import { nextui } from '@nextui-org/theme'
 module.exports = {
   darkMode: ['class'],
   content: [
-    './node_modules/@nextui-org/theme/dist/components/(button|input|image|spinner|select|dropdown|modal).js',
+    './node_modules/@nextui-org/theme/dist/components/(button|input|image|spinner|select|dropdown|modal|table).js',
     './src/components/**/*.{ts,tsx}',
     './src/app/**/*.{ts,tsx}',
   ],
