8ab24f8dfdf1ec6b4ff9a1039e4eb1ae950abb8d
maxxmorphis <max.x.morphis@gmail.com>
2023-10-18 17:20:48 +0900

    chore(): update beep contract

--- src/constants/beep.ts
+++ src/constants/beep.ts
@@ -3,7 +3,7 @@ import { Goerli, Ethereum } from '@thirdweb-dev/chains'
 import { chain } from '@/constants/chain'
 
 export const BeepContractAddress = {
-  [Goerli.chainId]: '0x86818Bf7d23FB9E588eFD5E927F6362E43244fa9',
+  [Goerli.chainId]: '0x1993d975E209D54e0bb9E4AC1aB999D339B6DEd3',
   [Ethereum.chainId]: '0x86818Bf7d23FB9E588eFD5E927F6362E43244fa9',
 } as { [key: number]: `0x${string}` }
 
