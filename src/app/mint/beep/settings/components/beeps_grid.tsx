c305bed2f5a469fd23414373340c082e4663f67f
AlexanderZhao <zhaomengzhe@gmail.com>
2023-10-19 18:08:18 +0800

    feat(): add not connected and no-beep UI in /mint/beep/settings

--- src/app/mint/beep/settings/components/beeps_grid.tsx
+++ src/app/mint/beep/settings/components/beeps_grid.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import Link from 'next/link'
-import { useChainId, useAddress, useContract, useOwnedNFTs } from '@thirdweb-dev/react'
+import { useConnectionStatus, useChainId, useAddress, useContract, useOwnedNFTs } from '@thirdweb-dev/react'
 import { Spinner } from '@nextui-org/spinner'
 
+import ConnectWalletButton from '@/components/connect_wallet_button'
 import BeepIframe from './beep_iframe'
 
 import { BeepContractAddress } from '@/constants/beep'
@@ -11,21 +12,52 @@ import { BeepContractAddress } from '@/constants/beep'
 import ChevronRight from 'public/icons/chevron-right.svg'
 
 const BeepsGrid = () => {
+  const connectionStatus = useConnectionStatus()
   const chainId = useChainId()
   const address = useAddress()
   const { contract } = useContract(chainId ? BeepContractAddress[chainId] : null)
   const { data, isLoading, error } = useOwnedNFTs(contract, address)
 
-  if (!chainId) return null
+  if (connectionStatus === 'disconnected') {
+    return (
+      <div className="min-h-[540px] flex flex-col items-center justify-center gap-4 tracking-wide">
+        <p className="font-[pixeloid-mono] text-sm text-center">Connect wallet to view your Beeps</p>
+        <ConnectWalletButton
+          className="!bg-transparent !font-medium !text-[#78edc1] [&>div>span:first-child]:text-[#78edc1]"
+          style={{ border: '1px solid #78edc1' }}
+        />
+      </div>
+    )
+  }
 
   if (error) {
-    return <p className="text-center">{(error as Error)?.message || 'Failed to load NFTs'}</p>
+    return (
+      <div className="min-h-[540px] flex items-center justify-center">
+        <p className="text-center">{(error as Error)?.message || 'Failed to load NFTs'}</p>
+      </div>
+    )
   }
 
   if (isLoading) {
     return (
-      <div className="min-h-[240px] flex items-center justify-center">
+      <div className="min-h-[540px] flex items-center justify-center">
         <Spinner color="default" />
+      </div>
+    )
+  }
+
+  if (!chainId) return null
+
+  if (!data?.length) {
+    return (
+      <div className="min-h-[540px] flex flex-col items-center justify-center gap-4 tracking-wide">
+        <p className="font-[pixeloid-mono] text-sm text-center">Looks like you don’t have any Beep bot</p>
+        <Link
+          className="px-8 py-2 font-medium text-[#78edc1] border border-[#78edc1] rounded-lg"
+          href="/mint/beep"
+        >
+          Mint Beep
+        </Link>
       </div>
     )
   }
