7c2a735c1263dcf21fd6e910fe425e7c5fd0270f
maxxmorphis <max.x.morphis@gmail.com>
2023-10-25 12:19:25 +0900

    chore(): a branch for polygon

--- src/app/mint/beep/settings/page.tsx
+++ src/app/mint/beep/settings/page.tsx
@@ -1,4 +1,9 @@
 import BeepsGrid from './components/beeps_grid'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Kiosk - Mint Beep Polygon',
+}
 
 const BeepSettings = () => (
   <main className="max-w-screen-2xl mx-auto px-4 md:px-8 py-2 pt-16 text-white">
