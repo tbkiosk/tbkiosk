43bb6406610a8aa9b5ff78754fe797766796ba11
AlexanderZhao <zhaomengzhe@gmail.com>
2023-11-01 11:16:48 +0800

    refactor(): migrate CHAIN_ID BEEP_CONTRACT_ADDRESS BEEP_TBA_IMPLEMENTATION_ADDRESS REGISTRY_ADDRESS to env

--- env.mjs
+++ env.mjs
@@ -8,9 +8,17 @@ export const env = createEnv({
     ALCHEMY_KEY: z.string().min(1),
     ETHERSCAN_KEY: z.string().min(1),
     POLYGONSCAN_KEY: z.string().min(1),
+    NEXT_PUBLIC_CHAIN_ID: z.string().min(1),
+    NEXT_PUBLIC_BEEP_CONTRACT_ADDRESS: z.string().min(1),
+    NEXT_PUBLIC_BEEP_TBA_IMPLEMENTATION_ADDRESS: z.string().min(1),
+    NEXT_PUBLIC_REGISTRY_ADDRESS: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_THIRDWEB_CLIENT_ID: z.string().min(1),
+    NEXT_PUBLIC_CHAIN_ID: z.string().min(1),
+    NEXT_PUBLIC_BEEP_CONTRACT_ADDRESS: z.string().min(1),
+    NEXT_PUBLIC_BEEP_TBA_IMPLEMENTATION_ADDRESS: z.string().min(1),
+    NEXT_PUBLIC_REGISTRY_ADDRESS: z.string().min(1),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
@@ -18,5 +26,9 @@ export const env = createEnv({
     ALCHEMY_KEY: process.env.ALCHEMY_KEY,
     ETHERSCAN_KEY: process.env.ETHERSCAN_KEY,
     POLYGONSCAN_KEY: process.env.POLYGONSCAN_KEY,
+    NEXT_PUBLIC_CHAIN_ID: z.string().min(1),
+    NEXT_PUBLIC_BEEP_CONTRACT_ADDRESS: z.string().min(1),
+    NEXT_PUBLIC_BEEP_TBA_IMPLEMENTATION_ADDRESS: z.string().min(1),
+    NEXT_PUBLIC_REGISTRY_ADDRESS: z.string().min(1),
   },
 })
