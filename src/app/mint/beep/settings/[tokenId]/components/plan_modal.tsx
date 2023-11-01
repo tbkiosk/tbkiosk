4b98402147a614ff64df3c5bf687a9d4c95ee7c6
AlexanderZhao <zhaomengzhe@gmail.com>
2023-11-01 14:10:31 +0800

    fix(): fix typo

--- src/app/mint/beep/settings/[tokenId]/components/plan_modal.tsx
+++ src/app/mint/beep/settings/[tokenId]/components/plan_modal.tsx
@@ -18,7 +18,7 @@ export const FREQUENCY_OPTIONS = [
   { frequency: '1', name: 'Daily' },
   { frequency: '7', name: 'Weekly' },
   { frequency: '14', name: 'Bi-Weekly' },
-  { frequency: '30', name: 'Montly' },
+  { frequency: '30', name: 'Monthly' },
 ]
 
 const PlanModal = ({
