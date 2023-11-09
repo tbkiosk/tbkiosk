aaddbc692a7c52e6c4f07e8662e2aecb9e611cba
AlexanderZhao <zhaomengzhe@gmail.com>
2023-11-09 16:08:44 +0800

    feat(): add SWAP_PRIVATE_KEY

--- src/utils/adminSwap.ts
+++ src/utils/adminSwap.ts
@@ -1,6 +1,9 @@
-import { env } from '../../env.mjs'
-import { Alchemy, Network, Utils, Wallet, BigNumber } from 'alchemy-sdk'
+import { Alchemy, Utils, Wallet, BigNumber } from 'alchemy-sdk'
+
 import { abi } from '@/utils/adminAbi'
+import { ALCHEMY_CONFIG } from '@/constants/alchemy'
+
+import { env } from 'env.mjs'
 
 type SwapDetail = {
   swapContract: string // TBA address
@@ -11,18 +14,12 @@ type SwapDetail = {
   beepFee: BigNumber // should include decimals
 }
 
-const settings = {
-  apiKey: env.ALCHEMY_KEY,
-  network: Network.ETH_GOERLI,
-}
-
-const alchemy = new Alchemy(settings)
+const alchemy = new Alchemy(ALCHEMY_CONFIG)
 const adminContract = env.NEXT_PUBLIC_ADMIN_CONTRACT_ADDRESS
 const chainId = env.NEXT_PUBLIC_CHAIN_ID
-const privateKey = '' //This should be the bot account that is used to call the admin contract
 
 export const swapSingleUser = async ({ swapContract, beepFee, gasFee, tokenOut, tokenIn, amountIn }: SwapDetail) => {
-  const wallet = new Wallet(privateKey, alchemy)
+  const wallet = new Wallet(env.SWAP_PRIVATE_KEY, alchemy)
   const feeData = await alchemy.core.getFeeData()
 
   const adminInterface = new Utils.Interface(abi)
@@ -47,7 +44,7 @@ export const swapSingleUser = async ({ swapContract, beepFee, gasFee, tokenOut, 
 }
 
 export const batchSwap = async (swapDetails: SwapDetail[]) => {
-  const wallet = new Wallet(privateKey, alchemy)
+  const wallet = new Wallet(env.SWAP_PRIVATE_KEY, alchemy)
   const feeData = await alchemy.core.getFeeData()
 
   const adminInterface = new Utils.Interface(abi)
@@ -70,3 +67,31 @@ export const batchSwap = async (swapDetails: SwapDetail[]) => {
 
   return await wallet.sendTransaction(transaction)
 }
+
+// swapSingleUser({
+//   swapContract: '0xD913fB80c3E691c9A44d603e3190435F40823087', // TBA address
+//   tokenIn: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
+//   tokenOut: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+//   amountIn: Utils.parseUnits('200', 6),
+//   beepFee: Utils.parseUnits('40', 6),
+//   gasFee: Utils.parseUnits('20', 6),
+// })
+
+// batchSwap([
+//   {
+//     swapContract: '0xD913fB80c3E691c9A44d603e3190435F40823087', // TBA address
+//     tokenIn: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
+//     tokenOut: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+//     amountIn: Utils.parseUnits('100', 6),
+//     beepFee: Utils.parseUnits('30', 6),
+//     gasFee: Utils.parseUnits('20', 6),
+//   },
+//   {
+//     swapContract: '0xD913fB80c3E691c9A44d603e3190435F40823087', // TBA address
+//     tokenIn: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
+//     tokenOut: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+//     amountIn: Utils.parseUnits('50', 6),
+//     beepFee: Utils.parseUnits('10', 6),
+//     gasFee: Utils.parseUnits('2', 6),
+//   },
+// ])
