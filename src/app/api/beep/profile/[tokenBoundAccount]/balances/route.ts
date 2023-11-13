2782be999ddf942aa6ddd2c88f732837a7b79a6b
AlexanderZhao <zhaomengzhe@gmail.com>
2023-11-13 14:59:17 +0800

    feat(): query all token balances

--- src/app/api/beep/profile/[tokenBoundAccount]/balances/route.ts
+++ src/app/api/beep/profile/[tokenBoundAccount]/balances/route.ts
@@ -1,29 +1,22 @@
 import { NextResponse } from 'next/server'
 import { Alchemy } from 'alchemy-sdk'
 
-import { USDC_CONTRACT_ADDRESS, WETH_CONTRACT_ADDRESS } from '@/constants/token'
+import { TOKENS_FROM, TOKENS_TO } from '@/constants/token'
 import { ALCHEMY_CONFIG } from '@/constants/alchemy'
-
-import { env } from 'env.mjs'
 
 export async function GET(request: Request, { params }: { params: { tokenBoundAccount: string } }) {
   const tokenBoundAccount = params.tokenBoundAccount
 
+  const tokens = { ...TOKENS_FROM, ...TOKENS_TO }
+
   try {
     const alchemy = new Alchemy(ALCHEMY_CONFIG)
 
-    const tokenBalances = await alchemy.core.getTokenBalances(tokenBoundAccount, [
-      USDC_CONTRACT_ADDRESS[+env.NEXT_PUBLIC_CHAIN_ID as 1 | 5 | 137],
-      WETH_CONTRACT_ADDRESS[+env.NEXT_PUBLIC_CHAIN_ID as 1 | 5 | 137],
-    ])
+    const balances = await alchemy.core.getTokenBalances(tokenBoundAccount, Object.keys(tokens))
 
-    const usdc = `${parseInt(tokenBalances.tokenBalances[0].tokenBalance?.toString() || '0') / 10 ** 6}`
-    const weth = `${parseInt(tokenBalances.tokenBalances[1].tokenBalance?.toString() || '0') / 10 ** 18}`
-
-    return NextResponse.json({
-      usdc,
-      weth,
-    })
+    return NextResponse.json(
+      Object.fromEntries(balances.tokenBalances.map(_balance => [_balance.contractAddress, `${parseInt(_balance.tokenBalance || '0')}`]))
+    )
   } catch (error) {
     return NextResponse.json({ error: (error as Error)?.message }, { status: 500 })
   }
