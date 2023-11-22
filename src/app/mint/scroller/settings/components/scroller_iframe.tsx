27d389ca70e7ef6f1d314e4df5841541dfa5da59
0xjims <145313241+0xjims@users.noreply.github.com>
2023-11-22 18:42:30 +0800

    feat(): added iframe (WIP)

--- src/app/mint/scroller/settings/components/scroller_iframe.tsx
+++ src/app/mint/scroller/settings/components/scroller_iframe.tsx
@@ -7,24 +7,34 @@ import clsx from 'clsx'
 
 import { env } from 'env.mjs'
 import { abi } from '@/utils/scrollerNft_abiEnumerable'
+import { tempSvg } from './base64Scroller'
+import { formatEther } from 'viem'
 
 const ScrollerIframe = ({ tokenId }: { tokenId: string | number }) => {
   const [loaded, setLoaded] = useState(false)
   const [svgString, setSvgString] = useState<string>('')
+  const [tbaBalance, setTbaBalance] = useState<string>('')
+  const [tba, setTba] = useState<any>({})
   const address = useAddress()
   const chainId = useChainId()
   const { contract } = useContract(chainId ? env.NEXT_PUBLIC_SCROLLER_NFT_CONTRACT_ADDRESS : null, abi)
 
   useEffect(() => {
-    const getSvgString = async () => {
+    const getTbaInfo = async () => {
       if (!contract || !address) return
-      const response = await contract.call('baseUri')
-      const svg = `data:image/svg+xml;base64,${response}`
-      setSvgString(svg)
+
+      // const baseUri = await contract.call('baseUri') // TODO
+      // const svg = `data:image/svg+xml;base64,${baseUri}`
+
+      const [tba, balance] = await contract.call('getTBA', [tokenId])
+
+      setTba(tba)
+      setTbaBalance(formatEther(balance).toString())
+      setSvgString(tempSvg)
       setLoaded(true)
     }
 
-    getSvgString()
+    getTbaInfo()
   }, [tokenId, contract, address])
 
   if (!chainId) return null
@@ -36,11 +46,23 @@ const ScrollerIframe = ({ tokenId }: { tokenId: string | number }) => {
           <Spinner color="default" />
         </div>
       )}
-      <div className={clsx('!h-full !w-full border-none rounded-[4%] opacity-0     cursor-pointer', loaded && 'opacity-100')}>
-        <img
-          src={svgString}
-          alt="iframe"
-        />
+      <div className={clsx('!h-full !w-full border-none rounded-[4%] opacity-0 cursor-pointer', loaded && 'opacity-100')}>
+        <div className="relative rounded-lg overflow-hidden w-64 h-64">
+          <div
+            className="absolute inset-0 bg-no-repeat bg-center bg-cover"
+            style={{ backgroundImage: `url("${tempSvg}")` }}
+          >
+            <div className="w-full text-center">
+              <div className="absolute top-4 w-full">
+                <p className="text-white text-2xl font-[pixel]">Scroller Pass #{tokenId}</p>
+              </div>
+              <div className="absolute bottom-4 w-full leading-[1]">
+                <p className="text-white">Balance: {tbaBalance} ETH</p>
+                <p className="text-white">Pref: {tba.preference}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
