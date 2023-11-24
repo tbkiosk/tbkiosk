5cfb9ef233ebfb1cc0ab14bdeda04ddb20608405
0xjims <145313241+0xjims@users.noreply.github.com>
2023-11-24 16:25:00 +0800

    fix(build): added sepolia to TWProvider

--- src/providers/thirdweb.tsx
+++ src/providers/thirdweb.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation'
 import { ThirdwebProvider as _ThirdwebProvider } from '@thirdweb-dev/react'
-import { Ethereum, Goerli, Polygon } from '@thirdweb-dev/chains'
+import { Ethereum, Goerli, Polygon, Sepolia } from '@thirdweb-dev/chains'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { env } from 'env.mjs'
@@ -27,7 +27,7 @@ const ThirdwebProvider = ({ children }: { children: React.ReactNode | React.Reac
       <_ThirdwebProvider
         activeChain={currentChainId}
         clientId={env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
-        supportedChains={[Ethereum, Goerli, Polygon]}
+        supportedChains={[Ethereum, Goerli, Polygon, Sepolia]}
       >
         {children}
       </_ThirdwebProvider>
